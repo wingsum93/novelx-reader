cd45c81a0588ab2308c1cf7d901f8a8f14174408
eric ho <eric.ws.ho@gtomato.com>
2020-08-20 15:43:31 +0800

    adding test

--- app/src/androidTest/java/com/ericho/example/NovelPageTest.kt
+++ app/src/androidTest/java/com/ericho/example/NovelPageTest.kt
@@ -9,39 +9,55 @@ import androidx.test.filters.LargeTest
 import androidx.test.internal.runner.junit4.AndroidJUnit4ClassRunner
 import androidx.test.rule.ActivityTestRule
 import com.ericho.example.ui.novel.NovelActivity
+import io.mockk.mockkClass
 import org.junit.After
-import org.junit.Before
+import org.junit.BeforeClass
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
-import org.koin.core.context.loadKoinModules
-import org.koin.core.context.unloadKoinModules
-import org.koin.test.KoinTest
+import org.koin.test.KoinTestRule
+import org.koin.test.mock.MockProviderRule
 
 
 @LargeTest
 @RunWith(AndroidJUnit4ClassRunner::class)
-class NovelPageTest : KoinTest {
+class NovelPageTest {
 
     @get:Rule
     var activityRule = ActivityTestRule(NovelActivity::class.java, false, false)
 
-    @Before
-    fun setUp() {
-        loadKoinModules(appModule)
-        activityRule.launchActivity(Intent())
+    @get:Rule
+    val koinTestRule = KoinTestRule.create {
+        // Your KoinApplication instance here
+        modules(appModule)
+    }
 
+    @get:Rule
+    val mockProvider = MockProviderRule.create { clazz ->
+        // Your way to build a Mock here
+        mockkClass(clazz)
     }
 
     @After
     fun tearDown() {
-        unloadKoinModules(appModule)
     }
 
     @Test
     fun whenActivityStartsLoginIsDisplayed() {
+//        startKoin {  }
+        activityRule.launchActivity(Intent())
+
         onView(withId(R.id.frameLayout)).check(matches(isDisplayed()))
         onView(withId(R.id.left)).check(matches(isDisplayed()))
         onView(withId(R.id.right)).check(matches(isDisplayed()))
+    }
+
+    companion object {
+        @BeforeClass
+        @JvmStatic
+        fun setUpAA() {
+
+
+        }
     }
 }
